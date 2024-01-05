0feb5c92c674772b44910be48e22550b49187ff5
thomis <thomas.steiner@ikey.ch>
2024-01-05 21:13:23 +0100

    more refactorings

--- cmd/check-certificate/check-certificate.go
+++ cmd/check-certificate/check-certificate.go
@@ -11,24 +11,28 @@ import (
 	"github.com/thomis/sensu-plugins-go/pkg/check"
 )
 
+type item struct {
+	Host string
+	Port int
+	Timeout int64
+	Expiry int64
+}
+
 func main() {
 	var (
-		host    string
-		port    int
-		timeout int64
-		expiry  int64
+		item item
 	)
 
 	c := check.New("CheckCertificate")
-	c.Option.StringVarP(&host, "host", "h", "localhost", "HOST")
-	c.Option.IntVarP(&port, "port", "P", 443, "PORT")
-	c.Option.Int64VarP(&timeout, "timeout", "t", 5, "TIMEOUT")
-	c.Option.Int64VarP(&expiry, "expiry", "e", 30, "EXPIRY warning in days")
+	c.Option.StringVarP(&item.Host, "host", "h", "localhost", "HOST")
+	c.Option.IntVarP(&item.Port, "port", "P", 443, "PORT")
+	c.Option.Int64VarP(&item.Timeout, "timeout", "t", 5, "TIMEOUT")
+	c.Option.Int64VarP(&item.Expiry, "expiry", "e", 30, "EXPIRY warning in days")
 	c.Init()
 
-	address := host + ":" + strconv.Itoa(port)
+	address := item.Host + ":" + strconv.Itoa(item.Port)
 
-	dialer := &net.Dialer{Timeout: time.Duration(timeout) * time.Second}
+	dialer := &net.Dialer{Timeout: time.Duration(item.Timeout) * time.Second}
 
 	conn, err := tls.DialWithDialer(dialer, "tcp", address, nil)
 	if err != nil {
@@ -37,7 +41,7 @@ func main() {
 	defer conn.Close()
 
 	// check if hostname matches with certificate
-	err = conn.VerifyHostname(host)
+	err = conn.VerifyHostname(item.Host)
 	if err != nil {
 		c.Error(err)
 	}
@@ -60,9 +64,9 @@ func main() {
 
 	// expiry warning in days
 	days_left := cert.NotAfter.Sub(now).Hours() / 24
-	if now.Add(time.Duration(expiry) * 24 * time.Hour).After(cert.NotAfter) {
+	if now.Add(time.Duration(item.Expiry) * 24 * time.Hour).After(cert.NotAfter) {
 		info := fmt.Sprintf("Certificate about to expire in less than %d days. Not After: %s (%0.1f days left)",
-			expiry, cert.NotAfter.Format("2006-01-02 15:04:05")+" UTC",
+			item.Expiry, cert.NotAfter.Format("2006-01-02 15:04:05")+" UTC",
 			days_left)
 		c.Warning(info)
 	}
