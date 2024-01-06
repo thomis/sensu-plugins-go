fb7cec8941c666a632de7091785001fe508ff2df
thomis <thomas.steiner@ikey.ch>
2024-01-06 08:33:58 +0100

    more tests

---
+++ pkg/check/check_test.go
@@ -0,0 +1,28 @@
+package check
+
+import (
+	"testing"
+	"fmt"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInit(t *testing.T) {
+	check := New("something")
+	check.Init()
+
+	assert.NotNil(t, check.Option)
+}
+
+func TestOk(t *testing.T) {
+	check := New("something")
+
+	go func() {
+			defer func() {
+				if r := recover(); r != nil {
+					assert.Equal(t, fmt.Sprintf("%v", r), "unexpected call to os.Exit(0) during test")
+				}
+			}()
+			check.Ok("whatever")
+		}()
+}
